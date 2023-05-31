a4da55fabcbf8db4d853cf6bc41f17aa829921a6
Cy Bunayog <cybunayog@gmail.com>
2023-05-30 20:47:15 -0700

    Add and organize assets

---
+++ src/app/assets/index.ts
@@ -0,0 +1,15 @@
+/* Photos */
+export {default as contact} from './contact.jpg';
+export {default as gallery} from './gallery_1.jpg';
+export {default as gallery2} from './gallery_2.jpg';
+export {default as gallery3} from './gallery_3.jpg';
+export {default as gallery4} from './gallery_4.jpg';
+export {default as gallery5} from './gallery_5.jpg';
+export {default as testImage} from './test.jpg';
+export {default as testImage2} from './test2.jpg';
+export {default as testImage3} from './test3.jpg';
+
+/* Icons */
+export {default as laIcon} from './la-icon.png';
+export {default as churchLogo} from './logo.png';
+export {default as oxnardIcon} from './oxnard-icon.png';
