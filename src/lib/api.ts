d51bb3f0c63faf1b38a82fed27ae1530429d8f30
Cy Bunayog <cybunayog@gmail.com>
2023-10-08 16:23:38 -0700

    Remove sendgrid lib and use custom api instead to handle call

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,13 +1,10 @@
-import sendgrid from '@sendgrid/mail';
-
 import { type SendGridOptions } from './types';
 
 export const sendEmail = async (data: SendGridOptions): Promise<void> => {
-  try {
-    sendgrid.setApiKey(process.env.SENDGRID_KEY);
-
-    await sendgrid.send(data);
-  } catch (error) {
-    console.error(error);
-  }
+  // try {
+  //   sendgrid.setApiKey(process.env.SENDGRID_KEY);
+  //   await sendgrid.send(data);
+  // } catch (error) {
+  //   console.error(error);
+  // }
 };
